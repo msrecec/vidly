62d7378249cbedfed74e2df0106d2b05f89e073b
Mislav Srecec <mislav.srecec3@gmail.com>
2020-10-01 19:53:08 +0200

    Added Mosh Solution

--- routes/customers.js
+++ routes/customers.js
@@ -1,44 +1,29 @@
-const mongoose = require('mongoose');
 const Joi = require('joi');
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-const customer = new mongoose.Schema({
-  isGold: {
-    type: Boolean,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 50,
-  },
-  phone: {
-    type: String,
-    required: true,
-    minlength: 4,
-    maxlength: 20,
-    trim: true,
-    validate: {
-      validator: function (v) {
-        return v && !isNaN(v);
-      },
-      message: 'Super wrong phone number bro',
+const Customer = mongoose.model(
+  'Customer',
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
     },
-  },
-});
-
-const Customer = mongoose.model('Customer', customer);
-
-function validateCustomer(customer) {
-  const schema = {
-    isGold: Joi.bool().required(),
-    name: Joi.string().min(1).max(50).required(),
-    phone: Joi.string().min(4).max(20).required(),
-  };
-  return Joi.validate(customer, schema);
-}
+    isGold: {
+      type: Boolean,
+      default: false,
+    },
+    phone: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+  })
+);
 
 router.get('/', async (req, res) => {
   const customers = await Customer.find().sort('name');
@@ -46,25 +31,17 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  try {
-    const { error } = validateCustomer(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+  const { error } = validateCustomer(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    let customer = new Customer({
-      isGold: req.body.isGold,
-      name: req.body.name,
-      phone: req.body.phone.replace(/\s/g, ''), // replacing spaces in case any
-    });
-    customer = await customer.save();
+  let customer = new Customer({
+    name: req.body.name,
+    isGold: req.body.isGold,
+    phone: req.body.phone,
+  });
+  customer = await customer.save();
 
-    res.send(customer);
-  } catch (ex) {
-    const errors = [];
-    for (field in ex.errors) {
-      errors.push(ex.errors[field].message);
-    }
-    res.status(400).send(errors.join('\n'));
-  }
+  res.send(customer);
 });
 
 router.put('/:id', async (req, res) => {
@@ -73,11 +50,18 @@ router.put('/:id', async (req, res) => {
 
   const customer = await Customer.findByIdAndUpdate(
     req.params.id,
-    { isGold: req.body.isGold, name: req.body.name, phone: req.body.phone },
+    {
+      name: req.body.name,
+      isGold: req.body.isGold,
+      phone: req.body.phone,
+    },
     { new: true }
   );
+
   if (!customer)
-    return res.status(404).send('The genre with the given ID was not found.');
+    return res
+      .status(404)
+      .send('The customer with the given ID was not found.');
 
   res.send(customer);
 });
@@ -86,7 +70,9 @@ router.delete('/:id', async (req, res) => {
   const customer = await Customer.findByIdAndRemove(req.params.id);
 
   if (!customer)
-    return res.status(404).send('The genre with the given ID was not found.');
+    return res
+      .status(404)
+      .send('The customer with the given ID was not found.');
 
   res.send(customer);
 });
@@ -95,8 +81,21 @@ router.get('/:id', async (req, res) => {
   const customer = await Customer.findById(req.params.id);
 
   if (!customer)
-    return res.status(404).send('The genre with the given ID was not found.');
+    return res
+      .status(404)
+      .send('The customer with the given ID was not found.');
+
   res.send(customer);
 });
+
+function validateCustomer(customer) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    phone: Joi.string().min(5).max(50).required(),
+    isGold: Joi.boolean(),
+  };
+
+  return Joi.validate(customer, schema);
+}
 
 module.exports = router;
