350195a2d0a09df3b71ba72a2a2edecd0f5df874
Mislav Srecec <mislav.srecec3@gmail.com>
2020-10-01 19:49:23 +0200

    Added Customers Route and Model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 
 mongoose
   .connect('mongodb://localhost/vidly')
@@ -11,6 +12,8 @@ mongoose
 app.use(express.json());
 
 app.use('/api/genres', genres);
+
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
